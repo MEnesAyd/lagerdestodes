ed03296778ad77ac4ac7aa3a50d87df76431d34f
Enes Aydin <m.aydin@gebhardt-group.com>
2022-07-04 11:56:51 +0200

    New Gui

--- src/lagerswingbing.java
+++ src/lagerswingbing.java
@@ -1,2 +1,19 @@
+import javax.swing.*;
+
 public class lagerswingbing {
+    private JButton button1;
+    private JPanel panel1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton storeButton;
+    private JButton outsourceButton;
+    private JButton restoreButton;
+    private JButton nextTaskButton;
+    private JButton DESTROYButton;
 }

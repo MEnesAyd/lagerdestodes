eb247c6a8b29023706d4265d058bec1ddb5afa33
Mirco Heck <m.heck@gebhardt-group.com>
2022-07-04 13:37:09 +0200

    Finished GUI design

--- src/Start.java
+++ src/Start.java
@@ -5,7 +5,11 @@ import javax.swing.*;
 public class Start extends JFrame {
 
     public static void main(String[] args) {
-        new GUI();
+        var window = new JFrame("Die Lager");
+        window.setContentPane(new lagerswingbing().getContentPanel());
+        window.setSize(800, 600);
+        window.setResizable(false);
+        window.setVisible(true);
     }
 
 
--- src/lagerswingbing.java
+++ src/lagerswingbing.java
@@ -1,19 +1,44 @@
 import javax.swing.*;
 
 public class lagerswingbing {
-    private JButton button1;
-    private JPanel panel1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
-    private JButton button6;
-    private JButton button7;
-    private JButton button8;
-    private JButton button9;
+    private JButton btn43B;
+    private JPanel cPanel;
+    private JButton btn41A;
+    private JButton btn43A;
+    private JButton btn42B;
+    private JButton btn41B;
+    private JButton btn42A;
+    private JButton btn32A;
+    private JButton btn22A;
+    private JButton btn12A;
     private JButton storeButton;
     private JButton outsourceButton;
     private JButton restoreButton;
     private JButton nextTaskButton;
     private JButton DESTROYButton;
+    private JButton btn11A;
+    private JButton btn11B;
+    private JButton btn12B;
+    private JButton btn13A;
+    private JButton btn13B;
+    private JButton btn21A;
+    private JButton btn21B;
+    private JButton btn22B;
+    private JButton btn23A;
+    private JButton btn23B;
+    private JButton btn31A;
+    private JButton btn31B;
+    private JButton btn32B;
+    private JButton btn33A;
+    private JButton btn33B;
+    private JLabel lblType;
+    private JLabel lblAttrA;
+    private JLabel lblAttrB;
+    private JLabel lblVal;
+    private JLabel lblTask;
+    private JComboBox comboBox1;
+
+
+    public JPanel getContentPanel() { return cPanel; }
+
 }
